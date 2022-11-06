4c009512f4de7c1ac65f84e2ab97f12f7bb874f1
kimgeunhoe <ghkim1031@naver.com>
2022-11-06 13:58:50 +0900

    Create BoardDAO.java

---
+++ gisikin/src/main/java/com/myweb/www/repository/BoardDAO.java
@@ -0,0 +1,20 @@
+package com.myweb.www.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.myweb.www.domain.PagingVO;
+import com.myweb.www.domain.BoardVO;
+
+public interface BoardDAO {
+	int insert(BoardVO bvo);
+	List<BoardVO> selectList(PagingVO pgvo);
+	BoardVO selectOne(long bno);
+	int update(BoardVO bvo);
+	int delete(long bno);
+	int selectTotalCount(PagingVO pgvo);
+	long selectLastBno();
+	int updateReadCount(@Param("bno") long bno, @Param("i") int i);
+	int updateCmtQty(@Param("bno") long bno, @Param("i") int i);
+}
